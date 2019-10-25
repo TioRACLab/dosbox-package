190d51a7d41646d24d7c0f7039455d96651abd97
Ricardo Coelho <tiorac@msn.com>
2019-10-25 20:30:30 -0300

    Project

---
+++ TioRACLab.DosBox.Package/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TioRACLab.DosBox.Package
+{
+    public class Class1
+    {
+        public void aaa()
+        {
+            using (FileStream zipToOpen = new FileStream(@"c:\users\exampleuser\release.zip", FileMode.Open))
+            {
+                using (ZipArchive archive = new ZipArchive(zipToOpen, ZipArchiveMode.Update))
+                {
+                    ZipArchiveEntry readmeEntry = archive.CreateEntry("Readme.txt");
+                    using (StreamWriter writer = new StreamWriter(readmeEntry.Open()))
+                    {
+                        writer.WriteLine("Information about this package.");
+                        writer.WriteLine("========================");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ TioRACLab.DosBox.PackageTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace TioRACLab.DosBox.PackageTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
